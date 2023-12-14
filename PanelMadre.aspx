b86f95bec31600c6c9a4285c4711c994ca55e0af
DamianRengifo <132175499+DamianRengifo@users.noreply.github.com>
2023-12-14 15:13:34 -0500

    Panel madre

---
+++ PanelMadre.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PanelMadre.aspx.cs" Inherits="ICBFWEB2.PanelMadre" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Panel Madre Comunitaria</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Bienvenido<asp:Label ID="lblUser" runat="server" Text=""></asp:Label> </h1>
+        </div>
+    </form>
+</body>
+</html>
---
+++ PanelMadre.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ICBFWEB2
+{
+    public partial class PanelMadre : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["idUsuario"] != null && Session["fk_idRol"].Equals(2)) {
+                lblUser.Text = Session["nombre"].ToString();
+            }
+            else
+            {
+                Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                Response.Cache.SetExpires(DateTime.UtcNow.AddMinutes(-1));
+                Response.Cache.SetNoStore();
+                Response.Redirect("Login.aspx");
+            }
+        }
+    }
+}
